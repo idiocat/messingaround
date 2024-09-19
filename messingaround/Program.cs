faabeca62e565613ef30209a6929a9eadca622e6
idiocy <idiocy.665@gmail.com>
2024-09-19 20:00:38 +0300

    Add project files.

---
+++ messingaround/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace messingaround
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string coolstring;
+            coolstring = "it's a string and it's cool";
+            Console.WriteLine(coolstring);
+            Console.ReadKey();
+        }
+    }
+}
